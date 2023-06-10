deffd6ca52ea607be451f84eb55dba1c1261d7a3
oleg <olegkrygin9@gmail.com>
2023-06-11 02:20:59 +0300

    Добавил проект - агент

---
+++ Agent/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+
+namespace Agent
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                await socket.ConnectAsync("127.0.0.1", 6543);
+                Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine($"Не удалось установить подключение с {socket.RemoteEndPoint}");
+            }
+        }
+    }
+}
