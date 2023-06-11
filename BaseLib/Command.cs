53225f1278158bbb9c7c80cf517b09eb6b5eaa2d
oleg <olegkrygin9@gmail.com>
2023-06-11 04:55:44 +0300

    Добавил библиотеку

---
+++ BaseLib/Command.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace BaseLib
+{
+    class CommandParams
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Operation { get; set; } = string.Empty;
+        public int Timer { get; set; }
+        public CommandParams() { }
+
+    }
+
+    class Command
+    {
+        public long CodeCommand { get; set; }
+        public CommandParams CommandParams { get; set; }
+
+        public Command(long code, CommandParams commandParams) { CodeCommand = code; CommandParams = commandParams; }
+
+        public override string ToString()
+        {
+            StringBuilder requestMessageStrBuild = new StringBuilder();
+            requestMessageStrBuild.Append(CodeCommand).Append(' ')
+                .Append(CommandParams.Number1).Append(' ')
+                .Append(CommandParams.Number2).Append(' ')
+                .Append(CommandParams.Operation).Append(' ')
+                .Append(CommandParams.Timer);
+            return requestMessageStrBuild.ToString();
+        }
+    }
+}
